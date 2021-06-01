4d71e2f6fd90f7467ff671c1722499709d9730d7
Loe2000 <loe@oregonstate.edu>
2021-05-31 22:44:53 -0700

    Code for Buzz output

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,8 +1,10 @@
 #empty method for now
 
 def fizzBuzz(a) :
-    if ( a % 3 == 0):
+    if ( a % 3 == 0 ):
         return "Fizz"
+    if ( a % 5 == 0 ):
+        return "Buzz"
     return
-    
+
 
