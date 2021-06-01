8474645857220b96780a0947c15efef2beb200f7
Loe2000 <loe@oregonstate.edu>
2021-05-31 23:05:31 -0700

    Wrote test case where year%100 = 0

--- test_leapYear.py
+++ test_leapYear.py
@@ -4,7 +4,8 @@ import leapYear
 class testCaseLeapYear(unittest.TestCase):
     def testmod400(self):
         self.assertEqual (leapYear.leapYear(2000), True)
-    
+    def testmod100(self):
+        self.assertEqual (leapYear.leapYear(1900), False)
     
 
 if __name__ == "__main__":
